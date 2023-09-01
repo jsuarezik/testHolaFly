88d61699bc9b19ed3a264ebcabee4e6840715fb3
Jesus Suarez <jisaac07@gmail.com>
2023-09-01 18:34:20 +0200

    feat(loggingMiddleware) Almacenar logs en DB

--- src/server/middlewares/loggingMiddleware.js
+++ src/server/middlewares/loggingMiddleware.js
@@ -1,9 +1,17 @@
+const db = require('../../app/db');
+
 const loggingMiddleware = (db) =>
     (req, res, next) => {
         const ip = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').split(',')[0].trim();
+        const method = req.method;
         const headers = JSON.stringify(req.headers);
         const originalUrl = req.originalUrl;
         // Persist this info on DB
+        db.logging.create( {
+            action: `${method} ${originalUrl}`,
+            header: headers,
+            ip,
+        })
         next();
     }
 
